fb08152730b4329506600f18f3cca2c12a7eaa88
mohemmed <mohemmedmohyb@gmail.com>
2023-09-20 11:34:29 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,13 @@ import 'package:flutter/material.dart';
 import 'mo.dart';
 import 'mohmm.dart';
 
-
-void main(){
-
+void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: rashed(),));
-
+    home: rashed(),
+  ));
 }
+
 class rashed extends StatelessWidget {
   const rashed({super.key});
 
@@ -20,11 +19,13 @@ class rashed extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("home"),
+        title: const Text("home"),
         backgroundColor: Colors.deepOrangeAccent,
         actions: [
-          IconButton(icon: Icon(Icons.abc),onPressed: () => {},),
-
+          IconButton(
+            icon: Icon(Icons.abc),
+            onPressed: () => {},
+          ),
         ],
       ),
       drawer: Drawer(
@@ -32,34 +33,34 @@ class rashed extends StatelessWidget {
         child: Column(
           children: [
             UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(backgroundImage:AssetImage("asset/ras.jpg"),),
-
-                accountName: Text("rashed"), accountEmail: Text("rashed@gmail.com")
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage("asset/ras.jpg"),
+                ),
+                accountName: Text("rashed"),
+                accountEmail: Text("rashed@gmail.com")),
+            Divider(
+              color: Colors.blue,
+              thickness: 12,
             ),
-    Divider(
-      color: Colors.blue,
-      thickness: 12,
-    ),
-            rash(context,"home",Icons.home,mob()),
-
-            rash(context,"call",Icons.call_end,mohemmed()),
-
+            rash(context, "home", Icons.home, mob()),
+            rash(context, "call", Icons.call_end, mohemmed()),
           ],
         ),
       ),
     );
   }
 }
-Padding rash(BuildContext context,tx,icon,nav){
-  return  Padding(
+
+Padding rash(BuildContext context, tx, icon, nav) {
+  return Padding(
     padding: const EdgeInsets.all(1.0),
     child: ListTile(
-    tileColor: Colors.cyanAccent,
-    title: Text(tx),
-    leading:Icon(icon) ,
-    trailing: Icon(Icons.arrow_forward_ios),
-    onTap: () =>
-      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>nav,))),
-
+        tileColor: Colors.cyanAccent,
+        title: Text(tx),
+        leading: Icon(icon),
+        trailing: Icon(Icons.arrow_forward_ios),
+        onTap: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => nav,
+            ))),
   );
 }
